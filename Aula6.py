4b0ceedee066ce39b3f5bdd47616a0c8013a8b05
Emily <alessandra283021@hotmail.com>
2021-05-11 20:55:02 -0300

    first commit

---
+++ Aula6.py
@@ -0,0 +1,9 @@
+def tomar_banho():
+    print("Tirar a roupa")
+    print("Abrir o chuveiro")
+    print("Se lavar")
+    print("fechar o chuveiro")
+    print("Se secar")
+    print("coloca uma roupa limpa")
+
+    tomar_banho()
